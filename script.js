604f9ffaabd6320acf2ec462986daf623d2517f9
Orient55 <122955312+Orient55@users.noreply.github.com>
2023-03-02 16:32:49 +0100

    -added counter to playGame function - added loop for best of 5 and declaring winner

--- script.js
+++ script.js
@@ -4,38 +4,60 @@ function getComputerChoice(min, max){
 }
 
 getComputerChoice(1, 3);
-let playerPick = prompt("Choose your weapon");
-let playerChoice = playerPick.toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+//let playerPick = prompt("Choose your weapon");
+//let playerChoice = playerPick.toLowerCase();
 //console.log(playerChoice);
-playGame(getComputerChoice(1, 3), playerChoice);
+//playGame(getComputerChoice(1, 3), playerChoice);
 
 function playGame(computerChoice, playerChoice){
     if (computerChoice == 1 && playerChoice == "rock"){
         console.log("Draw! Rock vs Rock");
     }
     if (computerChoice == 1 && playerChoice == "paper"){
+        playerScore++;
         console.log("Player Wins! Paper beats Rock");
     }
     if (computerChoice == 1 && playerChoice == "scissor"){
+        computerScore++;
         console.log("Computer Wins! Rock beats Scissor");
     }
     if (computerChoice == 2 && playerChoice == "rock"){
+        computerScore++;
         console.log("Computer Wins! Paper beats Rock");
     }
     if (computerChoice == 2 && playerChoice == "paper"){
-        console.log("Draw! Paer vs Paper");
+        console.log("Draw! Paper vs Paper");
     }
     if (computerChoice == 2 && playerChoice == "scissor"){
+        playerScore++;
         console.log("Player wins! Scissor beats Paper");
     }
     if (computerChoice == 3 && playerChoice == "rock"){
+        playerScore++;
         console.log("Player wins! Rock beats Scissor");
     }
     if (computerChoice == 3 && playerChoice == "paper"){
+        computerScore++;
         console.log("Computer wins! Scissor beats Paper");
     }
     if (computerChoice == 3 && playerChoice == "scissor"){
         console.log("Draw! Scissor vs Scissor")
     }
 
+}
+for (let i = 0; i < 5; i++){
+    let playerPick = prompt("Choose your weapon");
+    let playerChoice = playerPick.toLowerCase();
+    playGame(getComputerChoice(1, 3), playerChoice);1
+}
+if (playerScore > computerScore){
+    console.log("Player Wins!");
+}
+if (playerScore < computerScore){
+    console.log("Computer Wins!");
+}
+if (playerScore = computerScore){
+    console.log("Draw!");
 }
